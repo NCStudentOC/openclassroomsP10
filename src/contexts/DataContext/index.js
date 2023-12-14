f94204a963de8052cbd965fc54e8d8d366ef17f7
NCStudentOC <moulin.y@proton.me>
2023-12-14 14:50:50 +0100

    first commit

---
+++ src/contexts/DataContext/index.js
@@ -0,0 +1,74 @@
+import PropTypes from "prop-types";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+const DataContext = createContext({});
+
+export const api = {
+  loadData: async () => {
+    const json = await fetch("/events.json");
+    return json.json();
+  },
+};
+
+export const DataProvider = ({ children }) => {
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  //  déclaration d'état de "last"
+  const [last, setLast] = useState(null);
+
+  const getData = useCallback(async () => {
+
+    try {
+      // Appel à la méthode loadData de l'objet api pour charger les données
+      setData(await api.loadData());
+
+      // trie les événements par date
+      // et en récupérant le premier élément de la liste triée (qui est le dernier événement).
+      const lastEvt = data?.events.sort((evtA, evtB) =>
+        new Date(evtB.date) < new Date(evtA.date) ? -1 : 1)[0];
+
+
+      //  Stockage des données
+      setLast(lastEvt)
+
+    } catch (err) {
+      setError(err);
+    }
+  }, []);
+
+
+  useEffect(() => {
+    if (data) return;
+    getData();
+  });
+
+  return (
+    <DataContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{
+        data,
+        error,
+        // Ajout de last la dernière date 
+        // pour  y avoir accès partout
+        last,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+DataProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export const useData = () => useContext(DataContext);
+
+export default DataContext;
